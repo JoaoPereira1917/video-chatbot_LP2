636b8ce26a5df5bff667b22420e0a0cab0fd24da
João Pereira <asmodeustriple6@gmail.com>
2026-06-10 23:18:07 -0300

    Initial commit

---
+++ src/main/java/com/videochatbot/VideoChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.videochatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videochatbot/VideoChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videochatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
